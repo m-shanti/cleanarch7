6a071bd36aa4705a9eb1e515c867e17f32cc34a8
maciejw <maciejw@atena.pl>
2022-12-30 15:38:25 +0100

    =+Microsoft.Extensions.Logging.AzureAppServices

--- src/cleanarch7.Web/Program.cs
+++ src/cleanarch7.Web/Program.cs
@@ -57,7 +57,7 @@ builder.Host.ConfigureContainer<ContainerBuilder>(containerBuilder =>
   containerBuilder.RegisterModule(new DefaultInfrastructureModule(builder.Environment.EnvironmentName == "Development"));
 });
 
-//builder.Logging.AddAzureWebAppDiagnostics(); add this if deploying to Azure
+builder.Logging.AddAzureWebAppDiagnostics(); //add this if deploying to Azure
 
 var app = builder.Build();
 
